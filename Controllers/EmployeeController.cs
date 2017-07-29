42c31335d67a54ba3a6cb2b216d5b6a342351b72
DESKTOP-794718C\Aman-singla <honeybansal98@gmail.com>
2017-07-29 14:04:32 +0530

    push content to master for .Net Core App

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestWebApi.BusinessLogic;
+using TestWebApi.Model;
+
+namespace TestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            EmployeeDAL dal = new EmployeeDAL();
+            return dal.getEmployeeDetails();
+
+        }
+        [HttpPost]
+        public IEnumerable<Employee> Post(Employee employee)
+        {
+            EmployeeDAL dal = new EmployeeDAL();
+            var employeeList= dal.AddEmployee(employee);
+            return employeeList;
+        }
+
+    }
+}
